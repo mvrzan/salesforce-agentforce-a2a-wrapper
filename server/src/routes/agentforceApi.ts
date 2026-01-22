55f934980a1d9646b67c11138b447c1ffb37a430
mvrzan <mvrzan@icloud.com>
2026-01-22 11:52:46 -0500

    Add POST route for session initiation in agentforce API

--- server/src/routes/agentforceApi.ts
+++ server/src/routes/agentforceApi.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import startSession from "../controllers/startSession.ts";
 
 const agentforceApiRoutes = Router();
+
+agentforceApiRoutes.post("/api/v1/af/sessions/:sessionId", startSession);
 
 export default agentforceApiRoutes;
