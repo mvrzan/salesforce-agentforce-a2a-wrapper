18e3ac37f034c8826cc59147a2c8ca41d98dfc18
mvrzan <mvrzan@icloud.com>
2026-01-27 10:19:15 -0500

    add sendStreamingMessage route to agentforce API for streaming messages

--- server/src/routes/agentforceApi.ts
+++ server/src/routes/agentforceApi.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import startSession from "../controllers/startSession.ts";
 import deleteSession from "../controllers/deleteSession.ts";
+import sendStreamingMessage from "../controllers/sendStreamingMessage.ts";
 
 const agentforceApiRoutes = Router();
 
 agentforceApiRoutes.post("/api/v1/af/sessions/:sessionId", startSession);
 agentforceApiRoutes.delete("/api/v1/delete-session", deleteSession);
+agentforceApiRoutes.post("/api/v1/send-streaming-message", sendStreamingMessage);
 
 export default agentforceApiRoutes;
