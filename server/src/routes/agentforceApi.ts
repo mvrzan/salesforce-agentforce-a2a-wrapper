c8ef6b821d272c41e90a49fd3c96bd742561e4b8
mvrzan <mvrzan@icloud.com>
2026-01-27 09:48:25 -0500

    add deleteSession route to agentforce API for session termination

--- server/src/routes/agentforceApi.ts
+++ server/src/routes/agentforceApi.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import startSession from "../controllers/startSession.ts";
+import deleteSession from "../controllers/deleteSession.ts";
 
 const agentforceApiRoutes = Router();
 
 agentforceApiRoutes.post("/api/v1/af/sessions/:sessionId", startSession);
+agentforceApiRoutes.delete("/api/v1/delete-session", deleteSession);
 
 export default agentforceApiRoutes;
